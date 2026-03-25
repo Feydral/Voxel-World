edca8f8f79cd1a48591e95acd321a467974c5643
Feydral <lian.hable.feathertech@gmail.com>
2026-03-25 09:55:24 +0100

    feat(world): add example block types

--- src/world/block.rs
+++ src/world/block.rs
@@ -1,4 +1,20 @@
+#[repr(u16)]
 #[derive(Clone, Copy)]
 pub enum Block {
     Void,
+    Air,
+
+    DirtBlock,
+    GrassBlock,
+    StoneBlock,
+    CobbledStoneBlock,
+
+    GrassLayer {
+        height: u8,
+    },
+    Moos,
+    MoosLayer {
+        height: u8
+    },
+    Lichen,
 }
