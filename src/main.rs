c529a24e01086f11f7e95f3accaa674633ab7209
Feydral <lian.hable.feathertech@gmail.com>
2026-03-30 12:48:24 +0200

    feat: set up canvas and input for rendering

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,23 @@ mod world;
 mod math;
 mod canvas;
 
-use crate::{math::numerics::int3::Int3, world::WorldData};
+use crossterm::event::KeyCode;
+
+use crate::{canvas::{Canvas, input::Input}, math::numerics::int3::Int3, world::WorldData};
 
 fn main() {
     let mut world = WorldData::new();
     world.create_chunk(Int3::new(0, 0, 0), false);
+
+    let mut canvas = Canvas::new();
+    let mut input = Input::new();
+
+    loop {
+        let _ = input.update();
+        if input.is_key_pressed(KeyCode::Esc) { break; }
+
+        // code here...
+
+        canvas.render();
+    }
 }
